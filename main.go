46f74d71de6a00c54a11c8c878ca8ea55dd67cea
“a.heydarabadi” <aliheydarabadii@gmail.com>
2021-12-10 18:49:40 +0330

    add auto AutoMigrate

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"manageArticles_/api"
 	"manageArticles_/config"
+	"manageArticles_/internal/model"
 	"manageArticles_/internal/repo"
 	"manageArticles_/pkg/db"
 )
@@ -14,6 +15,7 @@ func main() {
 	userRepo := repo.NewUserRepositoryImpl(gorm)
 	articleRepo := repo.NewArticleRepositoryImpl(gorm)
 	handler := api.NewHandler(userRepo, articleRepo, config)
+	gorm.AutoMigrate(&model.User{}, &model.Article{})
 	log.Println("starting the web server")
 	handler.StartWebServer()
 	select {}
