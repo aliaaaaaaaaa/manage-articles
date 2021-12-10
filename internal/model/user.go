7e888064e1cd0476630aaa3b42276fbd31da8f7c
“a.heydarabadi” <aliheydarabadii@gmail.com>
2021-12-10 18:57:44 +0330

    add unique field for user table

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	Id        int       `json:"id"`
 	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"-"`
 	IsActive  bool      `json:"is_active"`
 	LastLogin time.Time `json:"last_login"`
